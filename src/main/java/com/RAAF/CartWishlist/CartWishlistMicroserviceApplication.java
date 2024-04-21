29edd971ce3f3d203c37b188d70e6740904341b9
alafty <mohamedelalafty@yahoo.com>
2024-04-21 12:18:12 +0200

    Initial commit

---
+++ src/main/java/com/RAAF/CartWishlist/CartWishlistMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.RAAF.CartWishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartWishlistMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartWishlistMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RAAF/CartWishlist/CartWishlistMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RAAF.CartWishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartWishlistMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
